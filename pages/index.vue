21b6968653185353fa7f20866ac47f00ff8b4f9f
theMistletoe <katachiwotodomete@gmail.com>
2020-05-11 12:34:53 +0900

    update: test commit for intagration

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <logo />
       <h1 class="title">
-        TODO-Practice
+        TODO-Practice for SLC
       </h1>
       <h2 class="subtitle">
         My splendid Nuxt.js project

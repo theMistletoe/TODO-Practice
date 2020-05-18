9052ba9bc6fcecc4f1c1ed6d7cbe041bbc2cf577
kazuki terashima <komasu39perc_tera@yahoo.co.jp>
2020-05-18 15:53:27 +0900

    詳細画面に付箋のタイトルと詳細の枠を表示する

--- pages/detail.vue
+++ pages/detail.vue
@@ -3,6 +3,14 @@
     <h1 class="font-semibold text-4xl text-blue-500">
       ＴＯ ＤＯ リスト詳細
     </h1>
+    <input
+      class="block shadow appearance-none border rounded py-2 px-3 text-gray-600 my-4 w-1/2 mx-auto"
+      type="text"
+    />
+    <textarea
+      class="block shadow appearance-none border rounded py-2 px-3
+    text-gray-600 my-4 w-1/2 h-64 mx-auto"
+    />
   </div>
 </template>
 

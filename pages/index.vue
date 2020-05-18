50e91778455d8aedc431af25d4b7a8db9d730400
kazuki terashima <komasu39perc_tera@yahoo.co.jp>
2020-05-18 11:39:28 +0900

    日本語変換時のエンターキーの挙動を考慮

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
     <input
       v-model="newTaskTitle"
       ref="inputTask"
-      v-on:keyup.enter="addTask"
+      v-on:keydown.enter="addTask"
       class="shadow appearance-none border rounded py-2 px-3 text-gray-600 my-4"
       type="text"
       name="task-title"

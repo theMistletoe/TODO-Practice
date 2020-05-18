9871c9d227b20f3224b1fdc8b991f9fdac33bce6
kazuki terashima <komasu39perc_tera@yahoo.co.jp>
2020-05-18 16:34:17 +0900

    一覧画面に戻るボタンとアクションを追加

--- pages/detail.vue
+++ pages/detail.vue
@@ -11,6 +11,13 @@
       class="block shadow appearance-none border rounded py-2 px-3
     text-gray-600 my-4 w-1/2 h-64 mx-auto"
     />
+    <n-link to="/">
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        戻る
+      </button>
+    </n-link>
   </div>
 </template>
 

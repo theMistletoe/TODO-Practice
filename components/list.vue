080cabb1d3005e69ec5e8f05195c49e91f74d3d5
kazuki terashima <komasu39perc_tera@yahoo.co.jp>
2020-05-19 10:40:22 +0900

    一覧と詳細のコンポーネントを作成、state項目を追加

---
+++ components/list.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="text-center container mx-auto">
+    <h1 class="font-semibold text-4xl text-blue-500">
+      ＴＯ ＤＯ リスト
+    </h1>
+
+    <input
+      v-model="newTaskTitle"
+      ref="inputTask"
+      v-on:keydown.enter="addTask"
+      class="shadow appearance-none border rounded py-2 px-3 text-gray-600 my-4"
+      type="text"
+      name="task-title"
+    />
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      @click="addTask"
+    >
+      追加
+    </button>
+
+    <ul>
+      <li
+        v-for="task in tasks"
+        class="py-2 px-8 border border-gray-400 text-gray-600 mx-auto w-64"
+      >
+        <n-link to="detail" class="text-blue-400 hover:text-blue-700">
+          {{ task.title }}
+        </n-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    tasks: [
+      { title: '仮タスクタイトル1', detail: '仮タスクタイトル1詳細' },
+      { title: '仮タスクタイトル2', detail: '仮タスクタイトル2詳細' },
+      { title: '仮タスクタイトル3', detail: '仮タスクタイトル3詳細' },
+      { title: '仮タスクタイトル4', detail: '仮タスクタイトル4詳細' }
+    ],
+    newTaskTitle: ''
+  }),
+  methods: {
+    addTask() {
+      if (this.newTaskTitle !== '') {
+        this.tasks.push({
+          title: this.newTaskTitle
+        })
+        this.newTaskTitle = ''
+      }
+      this.$refs.inputTask.focus()
+    }
+  }
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>

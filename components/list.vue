5ba5b7ce79a8edafb7f8bcbb3fa25ac17895cbe6
kazuki terashima <komasu39perc_tera@yahoo.co.jp>
2020-05-20 16:26:30 +0900

    一覧からタイトルの編集ができる

--- components/list.vue
+++ components/list.vue
@@ -21,13 +21,28 @@
     <ul>
       <li
         v-for="task in tasks"
-        class="py-2 px-8 border border-gray-400 text-gray-600 mx-auto w-64"
+        class="py-2 px-8 border border-gray-400 text-gray-600 mx-auto w-1/2 text-left"
       >
+        <div
+          v-if="!task.edit"
+          v-text="task.title"
+          v-on:click="task.edit = true"
+          class="inline-block"
+        ></div>
+        <input
+          v-if="task.edit"
+          v-auto-focus
+          type="text"
+          v-model="task.title"
+          @blur="task.edit = false"
+          @keydown.enter="task.edit = false"
+          class="inline-block"
+        />
         <button
-          class="text-blue-400 hover:text-blue-700"
+          class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           @click="displayDetail(task)"
         >
-          {{ task.title }}
+          詳細
         </button>
       </li>
     </ul>
--- pages/index.vue
+++ pages/index.vue
@@ -27,10 +27,26 @@ export default {
     return {
       currentView: 'list',
       tasks: [
-        { title: '仮タスクタイトル1', detail: '仮タスクタイトル1詳細' },
-        { title: '仮タスクタイトル2', detail: '仮タスクタイトル2詳細' },
-        { title: '仮タスクタイトル3', detail: '仮タスクタイトル3詳細' },
-        { title: '仮タスクタイトル4', detail: '仮タスクタイトル4詳細' }
+        {
+          title: '仮タスクタイトル1',
+          detail: '仮タスクタイトル1詳細',
+          edit: false
+        },
+        {
+          title: '仮タスクタイトル2',
+          detail: '仮タスクタイトル2詳細',
+          edit: false
+        },
+        {
+          title: '仮タスクタイトル3',
+          detail: '仮タスクタイトル3詳細',
+          edit: false
+        },
+        {
+          title: '仮タスクタイトル4',
+          detail: '仮タスクタイトル4詳細',
+          edit: false
+        }
       ],
       selectedTask: { title: '', detail: '' }
     }
@@ -39,7 +55,8 @@ export default {
     addTask(newTaskTitle) {
       if (newTaskTitle !== '') {
         this.tasks.push({
-          title: newTaskTitle
+          title: newTaskTitle,
+          edit: false
         })
       }
     },
